54fca4d1524890c61f9ab9bbbc4eaa0149d2903e
终极天谴怪 <1094410998@qq.com>
2023-09-26 18:45:02 +0800

    修复只能同时下载一个直播的问题

--- get_video_by_home_link_linux_arm64.py
+++ get_video_by_home_link_linux_arm64.py
@@ -16,7 +16,8 @@ def download(live_url, filename):
     print('开始下载', filename)
     # 过滤英文和汉字以外的字符
     filename = re.sub(r'[^\u4e00-\u9fa5a-zA-Z]', '', filename)
-    wget.download(live_url, '/media/sd/Download/' + time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())) +filename + '.flv')
+    wget.download(live_url, '/media/sd/Download/' + time.strftime('%Y-%m-%d-%H-%M-%S',
+                                                                  time.localtime(time.time())) + filename + '.flv')
     print(time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())) + '下载完成', filename)
     cmd = "ffmpeg -i /media/sd/Download/" + filename + ".flv -vcodec copy -acodec copy /media/sd/Download/" + filename + ".mp4"
     os.system(cmd)
@@ -45,6 +46,7 @@ with open("Tiktok_live_room_link_by_auto_get.txt", "r", encoding='utf-8') as fil
     live_room_dict = eval(file.read())
 while True:
     # for liver in home_links:
+    start_time = time.time()
     for home_link in home_links:
         # for live_link in live_links:
         try:
@@ -74,9 +76,9 @@ while True:
                         stream_is_get = True
                         time.sleep(2)
                         flv_name = str(request).split('.flv')[0]
-                        print(flv_name)
                         if flv_name not in live_name:
-                            print(time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time())) + "已获取"+liver+"流媒体：")
+                            print(time.strftime('%Y-%m-%d_%H:%M:%S',
+                                                time.localtime(time.time())) + "已获取" + liver + "流媒体：")
                             live_name.append(flv_name)
                             t = Thread(target=download, args=(str(request), liver))
                             t.start()
@@ -95,12 +97,13 @@ while True:
                             continue
         except NoSuchElementException:
             time.sleep(random.randint(5, 20))
-    print(home_link_dict)
-    print(live_room_dict)
+    end_time = time.time()
+    print("本次爬取列表中所有主播花费时间：", (end_time - start_time)/60, "分钟")
+    if len(home_link_dict) == len(home_links):
+        print('所有主播的直播间链接已获取完毕，可以考虑切换为直接访问直播间的模式')
     with open("Tiktok_home_link_by_auto_get.txt", "w", encoding='utf-8') as file:
         file.write(json.dumps(home_link_dict, ensure_ascii=False))
     file.close()
     with open("Tiktok_live_room_link_by_auto_get.txt", "w", encoding='utf-8') as file:
         file.write(json.dumps(live_room_dict, ensure_ascii=False))
     file.close()
-
